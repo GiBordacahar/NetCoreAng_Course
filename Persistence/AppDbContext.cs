d5e8740ec07b0439a21941fb768388aeb8bb101e
gibordacahar <gibordacahar@gmail.com>
2021-07-03 11:18:19 -0300

    Section 4

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -5,12 +5,20 @@ namespace NetCoreAng.Persistence
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models {get; set;}
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) 
           : base(options)
         {
         }
 
-        public DbSet<Make> Makes { get; set; }
-        public DbSet<Feature> Features { get; set; }
+        protected override void OnModelCreating(ModelBuilder mb) 
+        {
+            mb.Entity<VehicleFeature>().HasKey(vf => 
+            new { vf.VehicleId, vf.FeatureId });
+        }
+           
     }
 }
